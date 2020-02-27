653f56c3525943b52a5e85f23930a9fd61318efc
Alex Lewis <alexmichael.lewis@gmail.com>
2020-02-27 15:50:10 -0500

    reconfigure to only deploy dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const webpack = require('webpack')
-const path = require('path')
-const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const webpack = require('webpack');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname)
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         include: path.resolve(__dirname, 'src'),
-        use: [{
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['es2015', { modules: false }]
-            ]
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['es2015', { modules: false }]]
+            }
           }
-        }]
+        ]
       },
       {
         // Rules for SCSS files. Loaders run in reverse order
@@ -28,30 +29,34 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               url: false
             }
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sourceMap: true,
               precision: 8
             }
           }
-        ],
+        ]
       }
     ]
   },
   plugins: [
     new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
+      $: 'jquery',
+      jQuery: 'jquery',
       Popper: ['popper.js', 'default']
     }),
     new MiniCssExtractPlugin({
       filename: 'app.css'
-    })
+    }),
+    new CopyPlugin([
+      { from: 'src/index.html', to: '' },
+      { from: 'images', to: 'images' }
+    ])
   ]
 };
